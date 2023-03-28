7b094287965250d6f660f8a62cb1e23d25bbe089
satyabrata kar <satyabrata.kar011@outlook.com>
2023-03-28 11:10:26 +0530

    bootstrap area added

---
+++ NFS.WEB/Areas/Bootstrap/Controllers/BootstrapHulkController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NFS.WEB.Areas.Bootstrap.Controllers
+{
+    [Area("Bootstrap")]
+    public class BootstrapHulkController : Controller
+    {
+        public IActionResult ModelPopup()
+        {
+            return View();
+        }
+    }
+}
