0f4e22f3eaeca52d1c70cc849fb966f34e223c71
satyabrata kar <satyabrata.kar011@outlook.com>
2023-03-29 19:53:19 +0530

    api success

--- NFS.WEB/Controllers/WelcomeController.cs
+++ NFS.WEB/Controllers/WelcomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NFS.WEB.Models;
 
 namespace NFS.WEB.Controllers
 {
@@ -6,6 +8,8 @@ namespace NFS.WEB.Controllers
     {
         public IActionResult Index()
         {
+            var sss = SearchCustomers();
+
             //return View("~/Areas/Blogs/Views/Home/Index.cshtml");
             return View();
         }
@@ -13,6 +17,23 @@ namespace NFS.WEB.Controllers
         public IActionResult CarEMI()
         {
             return View();
+        }
+
+
+
+        private List<Employee>? SearchCustomers()
+        {
+            List<Employee>? emps = new List<Employee>();
+            string apiUrl = "https://localhost:7152/api/orax";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl + string.Format("/GetEmployees")).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                emps = JsonConvert.DeserializeObject<List<Employee>>(response.Content.ReadAsStringAsync().Result);
+            }
+
+            return emps;
         }
 
 
---
+++ NFS.WEB/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace NFS.WEB.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public int EmpPhoneNo { get; set; }
+        public DateTime EmpDOB { get; set; }
+    }
+}
